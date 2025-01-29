0e41dfa2c43f22d7510a441e99bc3cd8423ae8b3
DMAHW <dromarcaime10@gmail.com>
2025-01-29 20:36:38 +0100

    Update repository: Added new C++ and Python scripts for corner detection using Harris and FAST, and basic descriptor matching.

---
+++ cmake-build-debug/CMakeFiles/FeatureDetection.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FeatureDetection.dir/cpp/src/descriptors.cpp.o"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/descriptors.cpp.o.d"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/fast.cpp.o"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/fast.cpp.o.d"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/harris.cpp.o"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/harris.cpp.o.d"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/main.cpp.o"
+  "CMakeFiles/FeatureDetection.dir/cpp/src/main.cpp.o.d"
+  "FeatureDetection"
+  "FeatureDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FeatureDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
